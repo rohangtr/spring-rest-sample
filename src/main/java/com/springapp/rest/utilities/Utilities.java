15c8d8658fb85ea7652e81bbfd067891a9cc29fe
rohkushw <rohangtr@gmail.com>
2015-07-17 18:44:22 +0530

    Second Commit

--- src/main/java/com/springapp/rest/utilities/Utilities.java
+++ src/main/java/com/springapp/rest/utilities/Utilities.java
@@ -1,16 +1,7 @@
 package com.springapp.rest.utilities;
 
-import com.springapp.rest.repository.StaticEmployeeDAO;
 
 public class Utilities {
 
-	public static void main(String[] args) {
-		StaticEmployeeDAO sedao = new StaticEmployeeDAO();
-		
-		System.out.println(sedao.getEmployee(2));
-		System.out.println(sedao.deleteEmployee(2));
-		System.out.println(sedao.getEmployee(2));
-
-	}
 
 }
