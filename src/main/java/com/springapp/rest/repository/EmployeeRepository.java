47eff4255764e149890234d40fc666a5f35e2665
rohkushw <rohangtr@gmail.com>
2015-07-20 20:23:06 +0530

    Spring Data JPA 2

---
+++ src/main/java/com/springapp/rest/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.springapp.rest.repository;
+import org.springframework.data.repository.CrudRepository;
+
+import com.springapp.rest.model.Employee;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+}
