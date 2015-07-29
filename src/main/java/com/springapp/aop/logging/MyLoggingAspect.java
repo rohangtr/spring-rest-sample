eb1e86d9877cf9203bdaf41be7b0d57c56e10acc
rohkushw <rohangtr@gmail.com>
2015-07-29 16:03:44 +0530

    AOP Changes

--- src/main/java/com/springapp/aop/logging/MyLoggingAspect.java
+++ src/main/java/com/springapp/aop/logging/MyLoggingAspect.java
@@ -8,18 +8,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Aspect
-public class LoggingAspect {
+public class MyLoggingAspect {
 	
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 	
-	@Pointcut("within(com.springapp.rest..*)")
+	@Pointcut("within(com.springapp.rest.controller..*)")
 	public void loggingPointCut(){
 		
 	}
 	
-	@Around("loggingPointCut")
+	@Around("loggingPointCut()")
 	public Object empArroungAdvice(ProceedingJoinPoint proceedingJointPointCut){
-		log.info("Info From {},{}()", proceedingJointPointCut.getSignature().getDeclaringType(),proceedingJointPointCut.getSignature().getDeclaringTypeName());
+		
+		//log.info("Info From {},{}()", proceedingJointPointCut.getSignature().getDeclaringType(),proceedingJointPointCut.getSignature().getDeclaringTypeName());
+		System.out.println("Logging Aspect :: "+ proceedingJointPointCut.getSignature().getDeclaringTypeName()+"."+proceedingJointPointCut.getSignature().getName()+"()");
 		
 		Object result = null;
 		try{
@@ -29,6 +31,7 @@ public class LoggingAspect {
 		}
 		catch (Throwable e) {
 			log.error("Error occured. Reason {}",e.getCause());
+			System.out.println("Error occured. Reason "+ e.getCause());
 		}
 		return result;
 	}
