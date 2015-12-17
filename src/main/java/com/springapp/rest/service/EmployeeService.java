86ca7b7ccda73a26f3601dd8c5b1f479aa442d83
Rohan Kushwaha <rohangtr@gmail.com>
2015-12-17 12:12:24 +0530

    Added untracked files

---
+++ src/main/java/com/springapp/rest/service/EmployeeService.java
@@ -0,0 +1,58 @@
+package com.springapp.rest.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springapp.rest.model.Employee;
+import com.springapp.rest.repository.EmployeeRepository;
+import com.springapp.rest.utilities.Utilities;
+
+@Service
+public class EmployeeService {
+
+	@Autowired
+	EmployeeRepository employeeRepository;
+
+	public List<Employee> getEmployeeList() {
+		List<Employee> employees = Utilities.makeList(employeeRepository.findAll());
+		return employees;
+
+	}
+
+	public Employee getEmployee(Long id) {
+		Employee emp = employeeRepository.findOne(id);
+		return emp;
+	}
+
+	public String deleteEmployee(Long id) {
+		if (employeeRepository.exists(id)) {
+			employeeRepository.delete(id);
+			return "Employee deleted Successfully";
+		} else
+			return "Cannot find Employee";
+	}
+
+	public String createEmployee(Employee employee) {
+		employeeRepository.save(employee);
+		return "Employee created succesfully";
+	}
+
+	public boolean ifEmployeeExists(Long id) {
+
+		return employeeRepository.exists(id);
+
+	}
+
+	public String updateEmployee(Employee employee) {
+		if (employee.getId() != null) {
+			if (employeeRepository.exists(employee.getId())) {
+				employeeRepository.save(employee);
+				return "Employee updated succesfully";
+			} else
+				return "Employee does not succesfully";
+		}
+		return "Cannot find Employee";
+	}
+}
